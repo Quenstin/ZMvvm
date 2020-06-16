70f00702e48698f17621de00e752739f8b574c2a
zhuguoqing <zhuguoqing@yunxin.com>
2020-06-16 17:31:02 +0800

    baseFragment

---
+++ app/src/main/java/com/kotlin/zmvvm/base/view/BaseFragment.kt
@@ -0,0 +1,82 @@
+package com.kotlin.zmvvm.base.view
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
+import androidx.fragment.app.Fragment
+import com.kingja.loadsir.core.LoadService
+import com.kingja.loadsir.core.LoadSir
+import com.kotlin.zmvvm.R
+import com.kotlin.zmvvm.common.utils.ChangeThemeEvent
+import com.kotlin.zmvvm.common.utils.ColorUtil
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import java.util.*
+
+/**
+ * Created by zhgq on 2020/6/16
+ * Describe：fragment基类
+ */
+abstract class BaseFragment : Fragment() {
+    protected lateinit var loadService: LoadService<*>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(getLayoutId(), null)
+        loadService = LoadSir.getDefault().register(rootView) { reload() }
+        EventBus.getDefault().register(this)
+        return loadService.loadLayout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (Date().month + 1 == 4 && Date().date == 4) {
+            initStatusColor(ContextCompat.getColor(activity!!, R.color.colorGray666))
+        } else {
+            initStatusColor(0)
+        }
+        initView()
+        initData()
+    }
+
+    abstract fun initView()
+
+    open fun initData() {}
+
+    open fun reload() {
+        initData()
+    }
+
+    abstract fun getLayoutId(): Int
+
+    private fun initStatusColor(color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity!!.window.statusBarColor =
+                if (color == 0) ColorUtil.getColor(activity!!) else color
+        }
+        if (ColorUtils.calculateLuminance(Color.TRANSPARENT) >= 0.5) {
+            // 设置状态栏中字体的颜色为黑色
+            activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            // 跟随系统
+            activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        EventBus.getDefault().unregister(this)
+    }
+
+    @Subscribe
+    fun changeThemeEvent(event: ChangeThemeEvent) {
+        initStatusColor(0)
+    }
+}
---
+++ app/src/main/java/com/kotlin/zmvvm/base/view/BaseLifeCycleFragment.kt
@@ -0,0 +1,74 @@
+package com.kotlin.zmvvm.base.view
+
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.kingja.loadsir.callback.SuccessCallback
+import com.kotlin.zmvvm.base.viewmodel.BaseViewModel
+import com.kotlin.zmvvm.common.callback.EmptyCallBack
+import com.kotlin.zmvvm.common.callback.ErrorCallBack
+import com.kotlin.zmvvm.common.callback.LoadingCallBack
+import com.kotlin.zmvvm.common.state.State
+import com.kotlin.zmvvm.common.state.StateType
+import com.kotlin.zmvvm.common.utils.CommonUtil
+
+/**
+ * Created by zhgq on 2020/6/16
+ * Describe：生命周期管理
+ */
+abstract class BaseLifeCycleFragment<VM : BaseViewModel<*>> : BaseFragment() {
+
+    protected lateinit var mViewModel: VM
+
+    override fun initView() {
+        showLoading()
+        mViewModel = ViewModelProviders.of(this).get(CommonUtil.getClass(this))
+        mViewModel.loadState.observe(this,observer)
+        initDataObserver()
+
+    }
+
+    abstract fun initDataObserver()
+
+    private fun showLoading() {
+        loadService.showCallback(LoadingCallBack::class.java)
+    }
+
+    private fun showSuccess() {
+        loadService.showCallback(SuccessCallback::class.java)
+    }
+
+    private fun showError(msg: String) {
+        if (!TextUtils.isEmpty(msg)) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+        loadService.showCallback(ErrorCallBack::class.java)
+    }
+
+    open fun showTip(msg: String) {
+        if (!TextUtils.isEmpty(msg)) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+        loadService.showCallback(SuccessCallback::class.java)
+    }
+
+    open fun showEmpty() {
+        loadService.showCallback(EmptyCallBack::class.java)
+    }
+
+    private val observer by lazy {
+        Observer<State>{
+            it?.let {
+                when(it.code){
+                    StateType.SUCCESS -> showSuccess()
+                    StateType.LOADING -> showLoading()
+                    StateType.ERROR -> showTip(it.message)
+                    StateType.NETWORK_ERROR -> showError("网络异常")
+                    StateType.TIP -> showTip(it.message)
+                    StateType.EMPTY -> showEmpty()
+                }
+            }
+        }
+    }
+}
